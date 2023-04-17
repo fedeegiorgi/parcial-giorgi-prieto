cbb8f6525d1afe0eb36d7d0c591eec87b3533230
FedeeGiorgi <fgiorgi@mail.utdt.edu>
2023-04-17 20:13:39 -0300

    Grafico de denuncias x hora en los mas ruidosos

--- dataviz_3/script.js
+++ dataviz_3/script.js
@@ -1,5 +1,39 @@
-d3.dsv(";", 'astronautas.csv', d3.autoType).then(data => {
-  console.log(data);
+d3.dsv(";", '147_ruidos_molestos.csv', d3.autoType).then(data => {
+  const reclamosPorBarrio = d3.rollup(data, v => v.length, d => d.domicilio_barrio)
+  const top3 = Array.from(reclamosPorBarrio, ([key, value]) => ({barrio: key, reclamos: value}))
+    .sort((a, b) => d3.descending(a.reclamos, b.reclamos))
+    .slice(0, 3)
   
-  d3.select('#chart').append(() => chart);
-});
+  top3.forEach((d, i) => {
+    const dataFiltered = data.filter(e => e.domicilio_barrio === d.barrio)
+    const timeFormat = d3.timeFormat('%H')
+    const timeParse = d3.timeParse('%H:%M:%S')
+    const reclamosPorHora = d3.rollup(
+      dataFiltered,
+      v => v.length,
+      d => timeFormat(timeParse(d.hora_ingreso))
+    );
+
+    let chart = Plot.plot({
+      marks: [
+        Plot.barY(Array.from(reclamosPorHora, ([key, value]) => ({hora: key, reclamos: value})), {
+          x: 'hora',
+          y: 'reclamos',
+          fill: '#4773aa',
+        }),
+      ],
+      x: {
+        label: 'Hora',
+        domain: ["07", "08", "09", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "00", "01", "02", "03", "04", "05", "06"],
+      },
+      y: {
+        label: 'Cantidad de reclamos',
+      },
+      height: 400,
+      width: 600,
+      margin: 50,
+    })
+
+    d3.select(`#chart-${i}`).append(() => chart)
+  })
+})
